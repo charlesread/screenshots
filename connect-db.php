47bd8430fd044bc54ee63a2ef3154c742f7bd38a
ssherwood0627 <51965352+ssherwood0627@users.noreply.github.com>
2019-07-21 13:39:36 -0400

    Screenshots attempt 1

---
+++ connect-db.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+
+CONNECT-DB.PHP
+
+Allows PHP to connect to your database
+
+*/
+
+
+
+// Database Variables (edit with your own server information)
+
+$server = 'localhost';
+
+$user = 'root';
+
+$pass = '';
+
+$db = 'registration';
+
+
+
+// Connect to Database
+
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+
+
+
+
+?>
