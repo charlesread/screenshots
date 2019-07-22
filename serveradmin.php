397da506779cbb11607b69ebb4428f8c5a7eed86
ssherwood0627 <51965352+ssherwood0627@users.noreply.github.com>
2019-07-22 14:45:05 -0400

    Update serveradmin.php

--- serveradmin.php
+++ serveradmin.php
@@ -25,7 +25,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM admin WHERE username='$admin' AND password='$admin'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
