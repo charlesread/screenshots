0e487bb1fe357e7d832d694c7401ed0cc8926f90
ssherwood0627 <51965352+ssherwood0627@users.noreply.github.com>
2019-07-22 15:36:00 -0400

    Update serveradmin.php

--- serveradmin.php
+++ serveradmin.php
@@ -7,8 +7,7 @@ $email    = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
-
+$db = mysqli_connect('den1.mysql2.gear.host', 'carusers', 'Marlboro27!', 'carusers');
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
